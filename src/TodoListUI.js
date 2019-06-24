e4015e7204a11ba076a901bafa99ef6d6dc979d3
qiansongke <455988586@qq.com>
2019-06-24 11:05:43 +0800

    'add:stateless'

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,32 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Input, Button } from 'antd'
 import { List, Typography } from 'antd'
 
-class TodoListUI extends Component {
-    render() {
-        return (
+const TodoListUI = (props) => {
+    return (
+        <div>
             <div>
-                <div>
-                    <Input
-                        placeholder={this.props.inputValue}
-                        onChange={this.props.handleChange}
-                    />
-                    <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
-                </div>
-                <div>
-                    <List
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={(item, index) => (
-                            <List.Item onClick={(index) => {this.props.handledeleteItem(index)}}>
-                                <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                            </List.Item>
-                        )}
-                    />
-                </div>
+                <Input
+                    placeholder={props.inputValue}
+                    onChange={props.handleChange}
+                />
+                <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
             </div>
-        )
-    }
+            <div>
+                <List
+                    bordered
+                    dataSource={props.list}
+                    renderItem={(item, index) => (
+                        <List.Item onClick={(index) => { props.handledeleteItem(index) }}>
+                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        </div>
+    )
 }
+
+// class TodoListUI extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <div>
+//                     <Input
+//                         placeholder={this.props.inputValue}
+//                         onChange={this.props.handleChange}
+//                     />
+//                     <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
+//                 </div>
+//                 <div>
+//                     <List
+//                         bordered
+//                         dataSource={this.props.list}
+//                         renderItem={(item, index) => (
+//                             <List.Item onClick={(index) => { this.props.handledeleteItem(index) }}>
+//                                 <Typography.Text mark>[ITEM]</Typography.Text> {item}
+//                             </List.Item>
+//                         )}
+//                     />
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default TodoListUI;
